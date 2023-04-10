210a09f5d6e828111468f6d299cec2d13c6a0a89
Xejal <115703074+Xejal@users.noreply.github.com>
2023-04-10 15:14:36 +0400

    laboratoriya

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+
+
+function App() {
+  return (
+    <div className='container'>
+        <h1>Sharetrade</h1>
+    </div>
+  )
+}
+
+export default App;
